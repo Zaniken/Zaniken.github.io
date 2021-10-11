d49a8d38a8ea9cd37421eda107312df6b7bd7eeb
Zaniken <zanikencartoon@gmail.com>
2021-10-11 14:42:42 -0600

    Signed-off-by: Zaniken <zanikencartoon@gmail.com>

---
+++ scripts/slide.js
@@ -0,0 +1,31 @@
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 8000); // Change image every 2 seconds
+}
+function dropDown(){
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}}
+window.onload=carousel;
+window.onload=dropDown;
